2a3621bc9765f6340ff100aa8522c1901a9a14ed
EDEM GBADABIZO <edemgbadabizo@gmail.com>
2018-12-16 12:33:32 +0000

    comit client nav bar package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProduitComponent } from './produit/produit.component';
 import { ProduitMockService } from './produit/produit.mock.service';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ProduitComponent,
+    NavbarComponent,
+    SidebarComponent,
     
   ],
   imports: [
